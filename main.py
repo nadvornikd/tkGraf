f58bf455d66e62217ffdf7779265ec4b8f785404
nadvornikd <117067433+nadvornikd@users.noreply.github.com>
2023-03-20 09:37:51 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 from os.path import basename, splitext
 import tkinter as tk
+from tkinter import filedialog
+import pylab as pl
 
 # from tkinter import ttk
 
@@ -28,7 +30,7 @@ class MyEntry(tk.Entry):
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
-    name = "Foo"
+    name = "Fooo"
 
     def __init__(self):
         super().__init__(className=self.name)
@@ -38,41 +40,91 @@ class Application(tk.Tk):
         self.lbl.pack()
 
         self.fileFrame = tk.LabelFrame(self, text="soubor")
-        self.fileFrame.pack(padx=5, pady=5)
+        self.fileFrame.pack(padx=5, pady=5, fill="x")
         self.fileEntry = MyEntry(self.fileFrame)
-        self.fileEntry.pack()
-        self.fileBtn = tk.Button(self.fileFrame, text="...")
+        self.fileEntry.pack(fill="x")
+        self.fileBtn = tk.Button(self.fileFrame, text="...",command=self.fileSelect)
         self.fileBtn.pack(anchor="e")
 
-        self.dataformatVar = tk.IntVar()
-        self.radkyRbtn = tk.Radiobutton(self.fileFrame, text = "data jsou v radcich", variable=self.dataformatVar, value=0)
+        self.dataformatVar = tk.StringVar(value="RADEK")
+        self.radkyRbtn = tk.Radiobutton(
+            self.fileFrame, 
+            text = "data jsou v radcich", 
+            variable=self.dataformatVar, 
+            value="RADEK",
+        )
         self.radkyRbtn.pack(anchor="w")
-        self.sloupceRbtn = tk.Radiobutton(self.fileFrame, text="data jsou ve sloupcich", variable=self.dataformatVar, value=1)
+        self.sloupceRbtn = tk.Radiobutton(
+            self.fileFrame, 
+            text="data jsou ve sloupcich", 
+            variable=self.dataformatVar, 
+            value="SLOUPEC",
+        )
         self.sloupceRbtn.pack(anchor="w")
 
         self.grafFrame = tk.LabelFrame(self, text="Graf")
-        self.grafFrame.pack()
+        self.grafFrame.pack(fill="x")
         tk.Label(self.grafFrame, text="Titulek").grid(row=0, column=0)
-        self.titleEntry = tk.Entry(self.grafFrame)
+        self.titleEntry = MyEntry(self.grafFrame)
         self.titleEntry.grid(row=0, column=1)
 
         tk.Label(self.grafFrame, text="Popisek x").grid(row=1, column=0)
-        self.xlabelEntry = tk.Entry(self.grafFrame)
+        self.xlabelEntry = MyEntry(self.grafFrame)
         self.xlabelEntry.grid(row=1, column=1)
 
         tk.Label(self.grafFrame, text="Popisek y").grid(row=2, column=0)
-        self.ylabelEntry = tk.Entry(self.grafFrame)
+        self.ylabelEntry = MyEntry(self.grafFrame)
         self.ylabelEntry.grid(row=2, column=1)
 
         tk.Label(self.grafFrame, text="Mrizka").grid(row=3, column=0)
-        self.gridChck = tk.Checkbutton(self.grafFrame)
-        self.gridChck.grid(row=3, column=1, sticky="w")
+        self.gridVar = tk.BooleanVar(value=False)
+        self.gridCheck = tk.Checkbutton(self.grafFrame, variable=self.gridVar)
+        self.gridCheck.grid(row=3, column=1, sticky="w")
 
+        tk.Label(self.grafFrame, text="styl cary").grid(row=4, column=0)    
+        self.lineVar = tk.StringVar(value="none")
+        self.lineOpp = tk.OptionMenu(self.grafFrame, self.lineVar, "none", "-", "--", "-.", ":")
+        self.lineOpp.grid(row=4,column=1,sticky="w")
 
+        tk.Button(self,text="Kreslit", command=self.plotgraph).pack(anchor="w")
 
         self.btn = tk.Button(self, text="Quit", command=self.quit)
-        self.btn.pack()
+        self.btn.pack(anchor="e")
 
+    def fileSelect(self):
+        self.filename = filedialog.askopenfilename()
+        self.fileEntry.value = self.filename
+
+    def plotgraph(self):
+        with open(self.filename, "r") as f:
+            if self.dataformatVar.get() == "RADEK":
+                x = f.readline().split(";")
+                y = f.readline().split(";")
+                x = [float(i.replace(",",".")) for i in x]
+                y = [float(i.replace(",",".")) for i in y]
+            elif self.dataformatVar.get() == "SLOUPEC":
+                x = []
+                y = []
+                while True:
+                    line = f.readline()
+                    if line == "":
+                        break
+                    x1, y1 = line.split(";")
+                    x1 = float(x1.replace(",","."))
+                    y1 = float(y1.replace(",","."))
+                    x.append(x1)
+                    y.append(y1)
+
+
+        pl.plot(x,y, "o", linestyle = self.lineVar.get())
+        pl.xlabel(self.xlabelEntry.value)
+        pl.ylabel(self.ylabelEntry.value)
+        pl.title(self.titleEntry.value)
+        pl.grid(self.gridVar.get())
+        pl.show()
+
+
+ 
     def quit(self, event=None):
         super().quit()
 
