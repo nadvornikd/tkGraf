3de66638fb9bc4bd981a9ea7f4707bdd56e7b240
nadvornikd <117067433+nadvornikd@users.noreply.github.com>
2023-03-27 09:36:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -66,25 +66,38 @@ class Application(tk.Tk):
         self.grafFrame.pack(fill="x")
         tk.Label(self.grafFrame, text="Titulek").grid(row=0, column=0)
         self.titleEntry = MyEntry(self.grafFrame)
-        self.titleEntry.grid(row=0, column=1)
+        self.titleEntry.grid(row=0, column=1, columnspan=2)
 
         tk.Label(self.grafFrame, text="Popisek x").grid(row=1, column=0)
         self.xlabelEntry = MyEntry(self.grafFrame)
-        self.xlabelEntry.grid(row=1, column=1)
+        self.xlabelEntry.grid(row=1, column=1, columnspan=2)
 
         tk.Label(self.grafFrame, text="Popisek y").grid(row=2, column=0)
         self.ylabelEntry = MyEntry(self.grafFrame)
-        self.ylabelEntry.grid(row=2, column=1)
+        self.ylabelEntry.grid(row=2, column=1, columnspan=2)
 
         tk.Label(self.grafFrame, text="Mrizka").grid(row=3, column=0)
         self.gridVar = tk.BooleanVar(value=False)
         self.gridCheck = tk.Checkbutton(self.grafFrame, variable=self.gridVar)
         self.gridCheck.grid(row=3, column=1, sticky="w")
 
-        tk.Label(self.grafFrame, text="styl cary").grid(row=4, column=0)    
-        self.lineVar = tk.StringVar(value="none")
-        self.lineOpp = tk.OptionMenu(self.grafFrame, self.lineVar, "none", "-", "--", "-.", ":")
+        tk.Label(self.grafFrame, text="Styl cary").grid(row=4, column=0)    
+        self.lineVar = tk.StringVar(value="None")
+        self.lineOpp = tk.OptionMenu(self.grafFrame, self.lineVar, "None", "-", "--", "-.", ":")
         self.lineOpp.grid(row=4,column=1,sticky="w")
+
+        tk.Label(self.grafFrame, text="Marker").grid(row=5, column=0)    
+        self.markerVar = tk.StringVar(value="None")
+        self.markerOpp = tk.OptionMenu(self.grafFrame, self.markerVar, "None", *tuple(",.+*xXPo1234<>v^"))
+        self.markerOpp.grid(row=5,column=1,sticky="w")
+    
+        self.colorVar = tk.StringVar(value="Black")
+        self.colorOpp = tk.OptionMenu(self.grafFrame, self.colorVar, "Black", "Green", "Red", "Blue", "Pink")
+        self.colorOpp.grid(row=4,column=2,sticky="w")
+    
+        self.mcolorVar = tk.StringVar(value="Black")
+        self.mcolorOpp = tk.OptionMenu(self.grafFrame, self.mcolorVar, "Black", "Green", "Red", "Blue", "Pink")
+        self.mcolorOpp.grid(row=5,column=2,sticky="w")
 
         tk.Button(self,text="Kreslit", command=self.plotgraph).pack(anchor="w")
 
@@ -116,7 +129,13 @@ class Application(tk.Tk):
                     y.append(y1)
 
 
-        pl.plot(x,y, "o", linestyle = self.lineVar.get())
+        pl.plot(x,y,
+                linestyle = self.lineVar.get(),
+                marker = self.markerVar.get(), 
+                color = self.colorVar.get(), 
+                markeredgecolor = self.mcolorVar.get(),
+                markerfacecolor=self.mcolorVar.get(),
+                )
         pl.xlabel(self.xlabelEntry.value)
         pl.ylabel(self.ylabelEntry.value)
         pl.title(self.titleEntry.value)
